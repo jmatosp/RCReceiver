c3c168c8c0fd64da34066305ba279e7c04fff37d
Jose Pinto <bluecor@gmail.com>
2016-09-02 14:56:27 +0100

    updated to new change interrupt library

--- src/main.ino
+++ src/main.ino
@@ -1,32 +1,27 @@
-#include <PinChangeInt.h>
+// EnableInterrupt Simple example sketch. Demonstrates operation on a single pin of your choice.
+// See https://github.com/GreyGnome/EnableInterrupt and the README.md for more information.
+#include <EnableInterrupt.h>
 
-#define MY_PIN 5
+// Modify this at your leisure. Refer to https://github.com/GreyGnome/EnableInterrupt/wiki/Usage#Summary
+#define ARDUINOPIN 10
 
-volatile int pwm_value = 0;
-volatile int prev_time = 0;
-uint8_t latest_interrupted_pin;
+volatile uint16_t interruptCount=0; // The count will go back to 0 after hitting 65535.
 
-void rising() {
-    latest_interrupted_pin = PCintPort::arduinoPin;
-    PCintPort::attachInterrupt(latest_interrupted_pin, &falling, FALLING);
-    prev_time = micros();
-}
-
-void falling() {
-    latest_interrupted_pin = PCintPort::arduinoPin;
-    PCintPort::attachInterrupt(latest_interrupted_pin, &rising, RISING);
-    pwm_value = micros() - prev_time;
-    Serial.println(pwm_value);
+void interruptFunction() {
+  interruptCount++;
 }
 
 void setup() {
-    Serial.begin(115200);
-    Serial.print("PinChangeInt ReciverReading test");
-
-    pinMode(MY_PIN, INPUT);
-    digitalWrite(MY_PIN, HIGH);
-    PCintPort::attachInterrupt(MY_PIN, &rising, RISING);
+  Serial.begin(115200);
+  pinMode(ARDUINOPIN, INPUT_PULLUP);  // See http://arduino.cc/en/Tutorial/DigitalPins
+  enableInterrupt(ARDUINOPIN, interruptFunction, CHANGE);
 }
 
+// In the loop we just display interruptCount. The value is updated by the interrupt routine.
 void loop() {
+  Serial.println("---------------------------------------");
+  delay(1000);
+  Serial.print("Pin was interrupted: ");
+  Serial.print(interruptCount, DEC);
+  Serial.println(" times so far.");
 }
