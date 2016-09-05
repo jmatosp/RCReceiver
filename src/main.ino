508650218e3d2735bcfde82a2f56a929da46f3da
Jose Pinto <bluecor@gmail.com>
2016-09-05 14:32:14 +0100

    dont know

--- src/main.ino
+++ src/main.ino
@@ -1,20 +1,35 @@
-// EnableInterrupt Simple example sketch. Demonstrates operation on a single pin of your choice.
-// See https://github.com/GreyGnome/EnableInterrupt and the README.md for more information.
-#include <EnableInterrupt.h>
 
-// Modify this at your leisure. Refer to https://github.com/GreyGnome/EnableInterrupt/wiki/Usage#Summary
+#include <EnableInterrupt.h>    // See https://github.com/GreyGnome/EnableInterrupt and the README.md for more information.
+#include <TimerOne.h>           // http://playground.arduino.cc/Code/Timer1
+
 #define ARDUINOPIN 10
 
-volatile uint16_t interruptCount=0; // The count will go back to 0 after hitting 65535.
+volatile uint16_t interruptCount=0;     // The count will go back to 0 after hitting 65535.
 
-void interruptFunction() {
-  interruptCount++;
+unsigned int time[] = {0};
+
+void rising() {
+    Timer1.restart();           // set our stopwatch to 0
+    Timer1.start();             // and start it up
+    time[0] = Timer1.read();   // read the time since timer1 was restarted
+    interruptCount++;
+    enableInterrupt(ARDUINOPIN, falling, FALLING);
+}
+
+void falling() {
+    time[0] = Timer1.read();   // read the time since timer1 was restarted
+    Timer1.stop();
 }
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(ARDUINOPIN, INPUT_PULLUP);  // See http://arduino.cc/en/Tutorial/DigitalPins
-  enableInterrupt(ARDUINOPIN, interruptFunction, CHANGE);
+    Serial.begin(115200);
+
+    Timer1.initialize(2200);
+    Timer1.stop();
+    Timer1.restart();
+
+    pinMode(ARDUINOPIN, INPUT_PULLUP);
+    enableInterrupt(ARDUINOPIN, rising, RISING);
 }
 
 // In the loop we just display interruptCount. The value is updated by the interrupt routine.
